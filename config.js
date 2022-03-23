8e0f4ae295caa5f2cd396fe9113d2f5932a3fe2c
Hasif-bit <77277332+Hasif-bit@users.noreply.github.com>
2022-03-23 18:17:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ global.limitawal = {
     premium: "Infinity",
     free: 100
 }
-global.thumb = fs.readFileSync('./lib/Hisoka.pnj')
+global.thumb = fs.readFileSync('./lib/Hisoka.png')
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
